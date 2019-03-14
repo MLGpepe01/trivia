0cc37e08b1303732befab0a91fdff4bf75a6a3c9
Aldair <aldairgarcia360@gmail.com>
2019-03-14 13:42:50 -0400

    sadasd
    asdasdas

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import QuestionText from "./QuestionText.jsx";
 import Reset from "./Reset.jsx";
 import Answer from "./Answer.jsx";
+import Score from "./score.jsx";
 
 // import components
 
@@ -10,17 +11,25 @@ class Question extends Component {
     super(props);
     this.state = {
       questioncolor: this.props.currentQuestion.questioncolor,
-      question_text: this.props.currentQuestion.question_text
+      question_text: this.props.currentQuestion.question_text,
+      scorebored: this.props.scorebored
     };
   }
   handleClick(index) {
     var ca = this.props.currentQuestion.correct_choice_index;
+    var cnum = 0;
+
+    var rnum = 0;
     if (index === ca) {
       this.setState({ questiontext: "correct answer" });
       this.setState({ questioncolor: "green" });
+      this.setState({ uscore: cnum++ + 1 });
+      console.log(cnum);
     } else if (index !== ca) {
       this.setState({ questiontext: "Wrong!" });
       this.setState({ questioncolor: "red" });
+      this.setState({ uscore: rnum++ + 1 });
+      console.log(rnum);
     }
   }
 
@@ -54,6 +63,7 @@ class Question extends Component {
           />
         </div>
         <Reset />
+        <Score />
       </div>
     );
   }
---
+++ src/components/score.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+// import components
+
+class Score extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Score;
