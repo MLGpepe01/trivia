5151df62945c023f12f628a0e649e8302477c12d
MLGpepe01 <sliva@citypolyhs.org>
2019-03-17 15:30:15 -0400

    GOT THE TIMER TO WORK!!!!
    FINALLY< BUT IT STILL NEED TO MAKE THE PAGERELOAD CUZ ITS NPT DOINGF THAT RN.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom";
 import "./css/App.css";
 import App from "./components/App";
 ReactDOM.render(<App />, document.getElementById("root"));
+var timeleft = 10;
+var downloadTimer = setInterval(function() {
+  document.getElementById("progressBar").value = 10 - timeleft;
+  timeleft -= 1;
+  if (timeleft <= 0) clearInterval(downloadTimer);
+}, 1000);
+if (timeleft <= 0) window.location.reload(true);
