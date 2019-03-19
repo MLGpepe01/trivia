39671ca65df3501a041aaa0a48ae20dd1c724128
Aldair <aldairgarcia360@gmail.com>
2019-03-19 11:17:18 -0400

    csssaca
    ascasc

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./css/App.css";
 import App from "./components/App";
 ReactDOM.render(<App />, document.getElementById("root"));
+/*global $ */
 var timeleft = 10;
 var downloadTimer = setInterval(function() {
-  document.getElementById("progressBar").value = 10 - timeleft;
+  document.getElementById("progressBar").value = 11 - timeleft;
   timeleft -= 1;
   if (timeleft <= 0) clearInterval(downloadTimer);
+  console.log(timeleft);
+  console.log(timeleft);
+  $("#tmier").html(timeleft);
 }, 1000);
-if (timeleft <= 0) window.location.reload(true);
